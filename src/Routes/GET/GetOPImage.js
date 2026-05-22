75c21bcd8738254c94bbf69c63999be516ca6181
AbelMuro <abelmuro93@gmail.com>
2026-05-22 16:59:26 -0700

    commit

--- src/Routes/GET/GetOPImage.js
+++ src/Routes/GET/GetOPImage.js
@@ -2,18 +2,23 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../Config/MySQL/db.js');
 
-router.get('/get-account-name/:accountId', async (req, res) => {
+router.get('/get-image/:accountId', async (req, res) => {
     try{
         const accountId = req.params.accountId;
+
         const [results] = await db.execute(
-            'SELECT * FROM accounts WHERE id = ?',
+            'SELECT * FROM account_images WHERE account_id = ?', 
             [accountId]
         );
+
         if(!results.length)
-            return res.status(404).send('User not found');
-        
-        const accountName = results[0].company;
-        res.status(200).send(accountName);
+            return res.status(404).send("User doesn't have a photo");
+
+        const buffer = results[0]?.buffer;
+        const mimeType = results[0]?.mimeType;
+
+        res.set('Content-Type', mimeType);
+        res.status(200).send(buffer);
     }
     catch(error){
         const message = error.message;
